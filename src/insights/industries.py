c588a7f644c0f13c0d3e603062062e21bb238713
Solon <amaral.solon@hotmail.com>
2023-03-08 16:13:46 -0300

    Req 6 - Industry Type Filter

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -28,6 +28,11 @@ def get_unique_industries(path: str) -> List[str]:
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
+    industry_list = []
+    for job in jobs:
+        if job["industry"] == industry:
+            industry_list.append(job)
+    return industry_list
     """Filters a list of jobs by industry
 
     Parameters
