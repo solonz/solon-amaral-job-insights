c52ea997b5221a8fabd433cb953e3d79bb59f8e2
Solon <amaral.solon@hotmail.com>
2023-03-08 15:38:22 -0300

    Req 3 - Get Industries

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,7 +1,15 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
+    industries = read(path)
+    industries_types = []
+    for industrie in industries:
+        industrie_type_record = industrie["industry"]
+        if industrie_type_record not in industries_types:
+            industries_types.append(industrie_type_record)
+    return industries_types
     """Checks all different industries and returns a list of them
 
     Must call `read`
