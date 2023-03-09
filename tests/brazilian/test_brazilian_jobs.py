65057311788984add0fc06c74ec0308b51fe3a9f
Solon <amaral.solon@hotmail.com>
2023-03-09 10:49:03 -0300

    Req 11 - Brazilian Jobs (test)

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,11 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+
+path = "tests/mocks/brazilians_jobs.csv"
 
 
 def test_brazilian_jobs():
-    pass
+    jobs = read_brazilian_file(path)
+    for job in jobs:
+        assert job.keys() == {"title", "salary", "type"}
+
+        # https://www.w3schools.com/python/trypython.asp?filename=demo_ref_dictionary_keys
