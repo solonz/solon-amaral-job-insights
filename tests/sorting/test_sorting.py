b71d25daff308d84abd54fdb053de22d023a5b57
Solon <amaral.solon@hotmail.com>
2023-03-09 11:45:46 -0300

    Req 12 - Sorting (test)

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,25 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+
+
+mock = [
+    {
+        "min_salary": 8000,
+        "max_salary": 15000,
+        "date_posted": "07-03-2023",
+    },
+    {
+        "min_salary": 5000,
+        "max_salary": 12000,
+        "date_posted": "08-03-2023",
+    },
+    {
+        "min_salary": 6000,
+        "max_salary": 13000,
+        "date_posted": "09-03-2023",
+    },
+]
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(mock, "min_salary")
+    assert mock[0]["min_salary"] == 5000
