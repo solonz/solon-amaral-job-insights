ffc6c7a68b4f09b772a603afbc0bc91bdc96fa56
Solon <amaral.solon@hotmail.com>
2023-03-09 10:33:26 -0300

    Req 10 - Count Ocurrences (test)

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+
+path = "data/jobs.csv"
 
 
 def test_counter():
-    pass
+    assert count_ocurrences(path, "complex") == 2068
+    assert count_ocurrences(path, "release") == 178
